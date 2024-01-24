4d2dfc8df7e60d2b8775a5914453df24c25b23ef
Jonnie Möller <scarabeo85@msn.com>
2024-01-24 12:46:57 +0100

    Fixed localStorage

--- src/components/ToDoApp.tsx
+++ src/components/ToDoApp.tsx
@@ -4,25 +4,24 @@ import { ToDo } from "../moduls/ToDo";
 import { ToDos } from "./ToDos";
 
 export const ToDoApp = () => {
- 
-  const [toDos, setToDos] = useState<ToDo[]>([
-    new ToDo("WAKE UP!", false),
-    new ToDo("Turn on coffee", false),
-    new ToDo("Fry some eggs", false),
-    new ToDo("Turn on the computer", false),
-  ]);
+  const [toDos, setToDos] = useState<ToDo[]>(() => {
+    const savedToDos = localStorage.getItem("toDos");
+    if (savedToDos) {
+      return JSON.parse(savedToDos);
+    } else {
+      return [
+        new ToDo("WAKE UP!", false),
+        new ToDo("Turn on coffee", false),
+        new ToDo("Fry some eggs", false),
+        new ToDo("Turn on the computer", false),
+      ];
+    }
+  });
 
-
-useEffect (() => {
-  localStorage.setItem('toDos', JSON.stringify(toDos));
-}, [toDos]);
-
-useEffect (() => {
-  const toDos = JSON.parse(localStorage.getItem('toDos'));
-  if (toDos) {
-    setToDos(toDos)
-  }
-}, []);
+  useEffect(() => {
+    console.log("Saving to localStorage:", toDos);
+    localStorage.setItem("toDos", JSON.stringify(toDos));
+  }, [toDos]);
 
   const handleToDoTask = (name: string): void => {
     setToDos(
@@ -34,25 +33,27 @@ useEffect (() => {
         }
       })
     );
-    
   };
   const addANewToDo = (theNewToDo: string) => {
     setToDos([...toDos, new ToDo(theNewToDo, false)]);
   };
   const handleToDoDelete = (name: string) => {
-    setToDos(toDos.filter((toDo) => toDo.name !== name ));
-  }
+    setToDos(toDos.filter((toDo) => toDo.name !== name));
+  };
   const handleToDoSort = () => {
-
-    const sortedToDos = [...toDos].sort((a,b) => a.name.localeCompare(b.name));
-    setToDos(sortedToDos)
-  }
+    const sortedToDos = [...toDos].sort((a, b) => a.name.localeCompare(b.name));
+    setToDos(sortedToDos);
+  };
 
   return (
     <>
       <ToDoForm toDoForm={addANewToDo} sortToDos={handleToDoSort} />
       <ul>
-        <ToDos toDos={toDos} doToDo={handleToDoTask} deleteToDo={handleToDoDelete} />
+        <ToDos
+          toDos={toDos}
+          doToDo={handleToDoTask}
+          deleteToDo={handleToDoDelete}
+        />
       </ul>
     </>
   );
